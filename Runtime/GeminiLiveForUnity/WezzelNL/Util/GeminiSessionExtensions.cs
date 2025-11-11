f7f85f22a0d57c076f575d6f27c5c9e5af8ffcf6
streepje8 <wessel@wezzel.nl>
2025-11-11 01:10:51 +0100

    Added extra extension methods to showcase how to listen to the end of an ai response

---
+++ Runtime/GeminiLiveForUnity/WezzelNL/Util/GeminiSessionExtensions.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using WezzelNL.Gemini;
+
+namespace WezzelNL.Util
+{
+    internal class PlaybackFinishedHandler
+    {
+        private GeminiLiveSession Session { get; }
+        private StreamedAudioSource Source { get; }
+        private Action<GeminiLiveSession, StreamedAudioSource> OnFinishedHandler { get; }
+
+        public PlaybackFinishedHandler(GeminiLiveSession session, StreamedAudioSource source, Action<GeminiLiveSession, StreamedAudioSource> onFinishedHandler)
+        {
+            Session = session;
+            Source = source;
+            OnFinishedHandler = onFinishedHandler;
+        }
+
+        private bool turnEnded = false;
+        public Task OnTurnEnd(GeminiTurnEndEvent evt, CancellationToken ct)
+        {
+            turnEnded = true;
+            return Task.CompletedTask;
+        }
+        
+        public void OnFragmentFinish()
+        {
+            if (!turnEnded) return;
+            turnEnded = false;
+            OnFinishedHandler(Session, Source);
+        }
+    }
+    
+    public static class GeminiSessionExtensions
+    {
+        private static readonly Dictionary<StreamedAudioSource, GeminiLiveSession.GeminiEventHandler<GeminiSessionInteractionEvent>> PlaybackHandlers = new();
+        private static readonly Dictionary<Action<GeminiLiveSession, StreamedAudioSource>, PlaybackFinishedHandler> PlaybackFinishedHandlers = new();
+        public static void AddOnPlaybackFinishedHandler(this GeminiLiveSession session, StreamedAudioSource source, Action<GeminiLiveSession, StreamedAudioSource> onFinishedHandler)
+        {
+            session.RemoveOnPlaybackFinishedHandler(source, onFinishedHandler);
+            var handler = new PlaybackFinishedHandler(session, source, onFinishedHandler);
+            session.AddListener<GeminiTurnEndEvent>(handler.OnTurnEnd);
+            source.OnAudioFragmentFinishPlaying.AddListener(handler.OnFragmentFinish);
+            PlaybackFinishedHandlers[onFinishedHandler] = handler;
+        }
+        
+        public static void RemoveOnPlaybackFinishedHandler(this GeminiLiveSession session, StreamedAudioSource source, Action<GeminiLiveSession, StreamedAudioSource> onFinishedHandler)
+        {
+            if (PlaybackFinishedHandlers.Remove(onFinishedHandler, out var oldHandler))
+            {
+                session.RemoveListener<GeminiTurnEndEvent>(oldHandler.OnTurnEnd);
+                source.OnAudioFragmentFinishPlaying.RemoveListener(oldHandler.OnFragmentFinish);
+            }
+        }
+        
+        public static void AddPlayback(this GeminiLiveSession session, StreamedAudioSource source)
+        {
+            session.RemovePlayback(source);
+            var handler = CreatePlaybackHandler(source);
+            session.AddListener(handler);
+            PlaybackHandlers[source] = handler;
+        }
+
+        public static void RemovePlayback(this GeminiLiveSession session, StreamedAudioSource source)
+        {
+            if (PlaybackHandlers.Remove(source, out var oldHandler)) session.RemoveListener(oldHandler);
+        }
+        
+        private static GeminiLiveSession.GeminiEventHandler<GeminiSessionInteractionEvent> CreatePlaybackHandler(StreamedAudioSource source)
+        {
+            FloatDataStream stream = null;
+            return (evt, ct) =>
+            {
+                foreach (var part in evt.Interaction.Parts)
+                {
+                    if (part is not GeminiAudioInteractionPart geminiAudio) continue;
+                    if (!source.IsConnected) stream = source.Connect(geminiAudio.SampleRate);
+                    stream?.Write(geminiAudio.Samples);
+                }
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
--- Samples~/Usage/Scripts/IntegrationExample.cs
+++ Samples~/Usage/Scripts/IntegrationExample.cs
@@ -10,22 +10,41 @@ public class IntegrationExample : MonoBehaviour
     //Example on how to use the gemini api
     [field: SerializeField]public GeminiLiveConfiguration Configuration { get; private set; } = GeminiLiveConfiguration.Default;
     [field: SerializeField]public string ApiKey { get; private set; }
+    [field: SerializeField]public StreamedAudioSource StreamingSource { get; private set; }
     public GeminiLiveSession Session { get; private set; }
+    public Queue<string> TextResponseQueue { get; } = new();
+    
     
     void OnEnable()
     {
         Session = new GeminiLiveSession(GeminiAccessToken.Create(ApiKey), Configuration);
-        Session.AddListener<GeminiSessionInteractionEvent>(HandleInteractionAsync);
+        
+        //Session.AddListener<GeminiSessionInteractionEvent>(HandleInteractionAsync); You can use this instead of AddPlayback for more manual control or text-only generation
+        
         Session.AddListener<GeminiTranscribeEvent>(HandleTranscriptionAsync);
         Session.AddListener<GeminiServerClosedSessionEvent>(HandleServerClosure);
+        Session.AddListener<GeminiTurnEndEvent>(HandleTurnEnd);
+        Session.AddListener<GeminiGenerationCompleteEvent>(HandleGenerationComplete);
+        
+        //Extension methods for easily connecting the session to an audio source
+        Session.AddPlayback(StreamingSource); 
+        Session.AddOnPlaybackFinishedHandler(StreamingSource, OnAudioFinishPlaying);
+        
         AsyncDispatcher.DispatchNonBlocking(Session.StartSessionAsync, destroyCancellationToken);
     }
 
     void  OnDisable()
     {
-        Session.RemoveListener<GeminiSessionInteractionEvent>(HandleInteractionAsync);
+        //Session.RemoveListener<GeminiSessionInteractionEvent>(HandleInteractionAsync); You can use this instead of RemovePlayback for more manual control
+        
         Session.RemoveListener<GeminiTranscribeEvent>(HandleTranscriptionAsync);
         Session.RemoveListener<GeminiServerClosedSessionEvent>(HandleServerClosure);
+        Session.RemoveListener<GeminiTurnEndEvent>(HandleTurnEnd);
+        Session.RemoveListener<GeminiGenerationCompleteEvent>(HandleGenerationComplete);
+        
+        Session.RemovePlayback(StreamingSource);
+        Session.RemoveOnPlaybackFinishedHandler(StreamingSource, OnAudioFinishPlaying);
+        
         AsyncDispatcher.DispatchNonBlocking(Session.EndSessionAsync, destroyCancellationToken);
     }
     
@@ -38,34 +57,56 @@ public class IntegrationExample : MonoBehaviour
         return Task.CompletedTask;
     }
     
-    //Example on how to stream audio to an audio source
-    [field: SerializeField]public StreamedAudioSource StreamingSource { get; private set; }
-    public Queue<string> TextResponseQueue { get; } = new();
-    private FloatDataStream stream;
-    private Task HandleInteractionAsync(GeminiSessionInteractionEvent evt, CancellationToken ct)
-    {
-        foreach (var part in evt.Interaction.Parts)
-        {
-            switch (part)
-            {
-                case GeminiAudioInteractionPart geminiAudio:
-                {
-                    if(!StreamingSource.IsConnected) stream = StreamingSource.Connect(geminiAudio.SampleRate);
-                    stream.Write(geminiAudio.Samples);
-                } break;
-                case GeminiTextInteractionPart geminiText:
-                {
-                    TextResponseQueue.Enqueue(geminiText.Text);
-                } break;
-            }
-        }
-        return Task.CompletedTask;
-    }
-
+    //Example on how to use manual event handling instead of the AddPlayback method
+    // 
+    // private FloatDataStream stream;
+    // private Task HandleInteractionAsync(GeminiSessionInteractionEvent evt, CancellationToken ct)
+    // {
+    //     foreach (var part in evt.Interaction.Parts)
+    //     {
+    //         switch (part)
+    //         {
+    //             case GeminiAudioInteractionPart geminiAudio:
+    //             {
+    //                 if (!StreamingSource.IsConnected)
+    //                 {
+    //                     stream = StreamingSource.Connect(geminiAudio.SampleRate);
+    //                 }
+    //                 stream.Write(geminiAudio.Samples);
+    //             } break;
+    //             case GeminiTextInteractionPart geminiText:
+    //             {
+    //                 TextResponseQueue.Enqueue(geminiText.Text);
+    //             } break;
+    //         }
+    //     }
+    //     return Task.CompletedTask;
+    // }
     
     private Task HandleTranscriptionAsync(GeminiTranscribeEvent evt, CancellationToken ct)
     {
         TextResponseQueue.Enqueue(evt.Text);
+        return Task.CompletedTask;
+    }
+    
+    private Task HandleTurnEnd(GeminiTurnEndEvent evt, CancellationToken ct)
+    {
+        Debug.Log("Turn end!");
+        TextResponseQueue.Enqueue("\n\n");
+        return Task.CompletedTask;
+    }
+    
+    //This function is invoked everytime when the Session sends a TurnEnd signal and the streamedAudioSource finishes its buffer.
+    //Everytime the turn end signal is sent, all data from the current response has been sent to the audio source.
+    //This means that when that fragment finishes playing, the response finishes playing.
+    private void OnAudioFinishPlaying(GeminiLiveSession session, StreamedAudioSource source)
+    {
+        Debug.Log($"Audio on gameobject {source.gameObject.name} finished playing!");
+    }
+
+    private Task HandleGenerationComplete(GeminiGenerationCompleteEvent evt, CancellationToken ct)
+    {
+        Debug.Log("Generation Complete");
         return Task.CompletedTask;
     }
     
